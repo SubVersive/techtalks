e33195e17991f4033418eed32affcd6d73237891
Maksym Kondratiuk <Maksym.Kondratiuk@check24.de>
2020-11-06 13:24:30 +0100

    fix build

--- src/components/reveal-layout.jsx
+++ src/components/reveal-layout.jsx
@@ -1,29 +1,19 @@
 /* eslint-disable import/extensions */
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Reveal from 'reveal.js';
-
-import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
-
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/night.css';
-import 'reveal.js/plugin/highlight/zenburn.css';
+import loadable from '@loadable/component';
 
 import BaseLayout from './base-layout';
 
-const Layout = ({ children }) => {
-  useEffect(() => {
-    const deck = new Reveal({
-      plugins: [Highlight],
-    });
-    deck.initialize();
-  }, []);
-  return (
-    <BaseLayout>
-      <div style={{ height: '100vh' }}>{children}</div>
-    </BaseLayout>
-  );
-};
+// eslint-disable-next-line
+const MainReveal = loadable(() => import('./reveal/main'));
+
+const Layout = ({ children }) => (
+  <BaseLayout>
+    <MainReveal />
+    <div style={{ height: '100vh' }}>{children}</div>
+  </BaseLayout>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
